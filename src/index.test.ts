b101ade629aca7326eb81af63a6d7e55140d48d4
Michael Griscom <michaelgriscom@gmail.com>
2018-09-03 22:28:12 +0200

    wip

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,15 +1,20 @@
+import { expect } from "chai";
 import * as va from "virtual-alexa";
 
 describe("index tests", () => {
-    const alexa: va.VirtualAlexa = va.VirtualAlexa.Builder()
-        .handler("index.handler") // Lambda function file and name
-        .interactionModelFile("./models/en-US.json") // Path to interaction model file
-        .create();
+    let alexa: va.VirtualAlexa;
+    beforeEach(() => {
+        alexa = va.VirtualAlexa.Builder()
+            .handler("src/index.handler") // Lambda function file and name
+            .interactionModelFile("./models/en-US.json")
+            .create();
+    });
 
-    it("say hello", () => {
-        alexa.utter("hello").then((payload) => {
-            console.log("OutputSpeech: " + (payload as any).response.outputSpeech.ssml);
-            // Prints out returned SSML, e.g., "<speak> Welcome to my Skill </speak>"
+    it("say hello", (done) => {
+        alexa.utter("hello").then((result) => {
+            expect((result as any).response.outputSpeech.ssml).to.exist;
+            expect((result as any).response.outputSpeech.ssml).to.include("Hello World");
+            done();
         });
     });
 
