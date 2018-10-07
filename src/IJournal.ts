244d14a696da50705f79f670fa442b3ffcaade8b
Michael Griscom <michaelgriscom@gmail.com>
2018-10-07 18:32:49 +0200

    Using google spreadsheets (#2)
    * adding logic for creating spreadsheet
    
    * switching to use spreadsheets

--- src/IJournal.ts
+++ src/IJournal.ts
@@ -1,4 +1,4 @@
-import { google } from 'googleapis';
+import { GoogleDriveSpreadsheetWriter } from './GoogleDrive';
 
 export abstract class IJournal {
     abstract async createEntry(contents: string, accessToken: string): Promise<void>;
@@ -6,25 +6,28 @@ export abstract class IJournal {
 
 export class ConsoleJournal implements IJournal {
     async createEntry(entry: string, accessToken: string): Promise<void> {
-        const oauth2Client = new google.auth.OAuth2();
-        oauth2Client.setCredentials({ access_token: accessToken });
-        const drive = google.drive({
-            version: 'v3',
-            auth: oauth2Client
-        });
-        const res = await drive.files.create({
-            requestBody: {
-                name: 'DreamSkill',
-                mimeType: 'text/plain'
-            },
-            media: {
-                mediaType: 'text/plain',
-                body: entry
-            }
-        }, null);
+        const spreadSheet = new GoogleDriveSpreadsheetWriter(accessToken);
+        await spreadSheet.createEntry(entry);
 
-        console.log("creating entry" + entry);
-        console.log("drive response", res);
+        // const oauth2Client = new google.auth.OAuth2();
+        // oauth2Client.setCredentials({ access_token: accessToken });
+        // const drive = google.drive({
+        //     version: 'v3',
+        //     auth: oauth2Client
+        // });
+        // const res = await drive.files.create({
+        //     requestBody: {
+        //         name: 'DreamSkill',
+        //         mimeType: 'text/plain'
+        //     },
+        //     media: {
+        //         mediaType: 'text/plain',
+        //         body: entry
+        //     }
+        // }, null);
+
+        // console.log("creating entry" + entry);
+        // console.log("drive response", res);
     }
 }
 
